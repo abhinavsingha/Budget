0a568679bae0469434200d2e1ffe6693e221c198
Abhinav <abhinavsingha@outlook.com>
2023-05-11 00:27:04 +0530

    cda parking subhead Type

--- src/app/cda-parking-report/cda-parking-report.component.ts
+++ src/app/cda-parking-report/cda-parking-report.component.ts
@@ -103,6 +103,7 @@ export class CdaParkingReportComponent implements OnInit {
   cdaUnitList: any[] = [{ cda: 'All CDA' }, { cda: 'Mumbai CDA' }];
   amountType: any;
   allocationType: any;
+  subHeadType: any;
 
   ngOnInit(): void {
     $.getScript('assets/js/adminlte.js');
@@ -112,6 +113,27 @@ export class CdaParkingReportComponent implements OnInit {
     this.getAmountType();
     this.getAllocationTypeData();
     this.getDashboardData();
+    this.getSubHeadType();
+  }
+  getSubHeadType() {
+    this.apiService.getApi(this.cons.api.getSubHeadType).subscribe({
+      next: (v: object) => {
+        this.SpinnerService.hide();
+        let result: { [key: string]: any } = v;
+
+        if (result['message'] == 'success') {
+          this.subHeadType = result['response'];
+        } else {
+          this.common.faliureAlert('Please try later', result['message'], '');
+        }
+      },
+      error: (e) => {
+        this.SpinnerService.hide();
+        console.error(e);
+        this.common.faliureAlert('Error', e['error']['message'], 'error');
+      },
+      complete: () => console.info('complete'),
+    });
   }
 
   @ViewChild('authFileInput') authFileInput: any;
