4bbd00c2ed2aaa475e95e88b4845ef7e609c5286
Abhinav <abhinavsingha1503@gmail.com>
2023-06-01 12:18:33 +0530

    revision

--- src/app/budget-approver/budget-approver.component.ts
+++ src/app/budget-approver/budget-approver.component.ts
@@ -284,7 +284,7 @@ export class BudgetApproverComponent implements OnInit {
     this.multipleCdaParking = [];
     this.multipleCdaParking.push(new MultiCdaParking());
     if(data.revisedAmount!=undefined)
-      this.totalAmountToAllocateCDAParking = parseFloat(data.allocationAmount)+parseFloat(data.revisedAmount).toFixed(4);
+      this.totalAmountToAllocateCDAParking = (parseFloat(data.allocationAmount)+parseFloat(data.revisedAmount)).toFixed(4);
     else
       this.totalAmountToAllocateCDAParking = data.allocationAmount;
     this.balancedRemaingCdaParkingAmount = this.totalAmountToAllocateCDAParking;
