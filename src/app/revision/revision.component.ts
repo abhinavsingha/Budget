20ab4b6e82d31d86116e3e65641c4a0eba51a80d
Abhinav <abhinavsingha1503@gmail.com>
2023-07-19 14:15:57 +0530

    revision verification

--- src/app/revision/revision.component.ts
+++ src/app/revision/revision.component.ts
@@ -885,4 +885,20 @@ export class RevisionComponent {
       this.common.warningAlert('CDA amount mismatch','CDA total does not match the amount','');
     }
   }
+  confirmModel() {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, submit it!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.saveRevisionData();
+      }
+    });
+  }
+
 }
